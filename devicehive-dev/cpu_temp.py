8d1266fb042e637c679caac8c7cc72c9ca370aef
Johan Henselmans <johan@netsense.nl>
2021-08-18 20:25:37 +0200

    cpu_temp

---
+++ devicehive-dev/cpu_temp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from subprocess import check_output
+from re import findall
+
+def get_temp():
+    temp = check_output(["vcgencmd","measure_temp"]).decode("UTF-8")
+    return(findall("\d+\.\d+",temp)[0])
+
+#cpu_temp=get_temp()
+#print(cpu_temp)
